4c23d6570912b643f1932e6206bd40c2cabf9e4e
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-11-02 16:06:17 +0530

    Setup sign_in route

--- src/router.jsx
+++ src/router.jsx
@@ -1,7 +1,9 @@
 import { createBrowserRouter, redirect } from "react-router-dom";
 import App from "./App";
 import SignUpPage from "./components/SignUpPage";
+import SignInPage from "./components/SignInPage";
 import signUp from "./actions/sign-up";
+import signInUser from "./actions/sign-in-user";
 
 const baseUrl = import.meta.env.VITE_BACKEND_BASE_URL;
 const router = createBrowserRouter([
@@ -23,6 +25,22 @@ const router = createBrowserRouter([
           }
 
           return redirect("/sign-in");
+        },
+      },
+      {
+        path: "sign_in",
+        element: <SignInPage />,
+        action: async ({ request }) => {
+          const formData = Object.fromEntries(
+            (await request.formData()).entries(),
+          );
+
+          const { userId, errors } = await signInUser(baseUrl, formData);
+          if (errors) {
+            return errors;
+          }
+
+          return redirect(`/${userId}`);
         },
       },
     ],
