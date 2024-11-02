63d5367b48501dceb3995bec43dcdaf48fa61b00
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-11-02 16:05:24 +0530

    Define signInUser function

---
+++ src/actions/sign-in-user.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+import setAccessToken from "../utils/set-access-token";
+
+async function signInUser(baseUrl, formData) {
+  try {
+    const response = await axios.post(`${baseUrl}/sign_in`, formData);
+    const { userId, accessToken } = response.data;
+    setAccessToken(accessToken);
+
+    return { userId };
+  } catch (error) {
+    return { errors: error.response.data.errors };
+  }
+}
+
+export default signInUser;
