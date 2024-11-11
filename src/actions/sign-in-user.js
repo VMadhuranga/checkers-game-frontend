1939e1dac6aa317b39733bae16b7e28424f593f9
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-11-11 08:34:47 +0530

    Set withCredential property

--- src/actions/sign-in-user.js
+++ src/actions/sign-in-user.js
@@ -3,7 +3,9 @@ import setAccessToken from "../utils/set-access-token";
 
 async function signInUser(baseUrl, formData) {
   try {
-    const response = await axios.post(`${baseUrl}/sign_in`, formData);
+    const response = await axios.post(`${baseUrl}/sign_in`, formData, {
+      withCredentials: true,
+    });
     const { userId, accessToken } = response.data;
     setAccessToken(accessToken);
 
