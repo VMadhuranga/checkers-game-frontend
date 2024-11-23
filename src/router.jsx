cd8cbc331089691c80d6d1a31c2da5cab5e92d33
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-11-23 20:22:15 +0530

    Setup profile/edit path in router

--- src/router.jsx
+++ src/router.jsx
@@ -5,6 +5,7 @@ import SignInPage from "./components/SignInPage";
 import User from "./components/User";
 import MainMenuPage from "./components/MainMenuPage";
 import ProfilePage from "./components/ProfilePage";
+import EditProfilePage from "./components/EditProfilePage";
 import signInUser from "./actions/sign-in-user";
 import signUpUser from "./actions/sign-up-user";
 import getFormData from "./utils/get-form-data";
@@ -12,6 +13,8 @@ import getUserById from "./loaders/get-user-by-id";
 import refreshToken from "./loaders/refresh-token";
 import signOutUser from "./loaders/sign-out-user";
 import deleteUserById from "./loaders/delete-user-by-id";
+import updateUsername from "./actions/update-username";
+import updatePassword from "./actions/update-password";
 
 const baseUrl = import.meta.env.VITE_BACKEND_BASE_URL;
 const router = createBrowserRouter([
@@ -80,6 +83,38 @@ const router = createBrowserRouter([
               const { user_id } = params;
               await deleteUserById(baseUrl, user_id);
               return redirect("/sign_up");
+            },
+          },
+          {
+            path: "profile/edit",
+            element: <EditProfilePage />,
+            action: async ({ request, params }) => {
+              const { user_id } = params;
+              const formData = await getFormData(request);
+
+              if (Object.hasOwn(formData, "new_username")) {
+                const errors = await updateUsername(baseUrl, user_id, formData);
+                if (errors) {
+                  return { errors };
+                }
+
+                return { usernameUpdated: true };
+              }
+
+              if (
+                Object.hasOwn(formData, "old_password") &&
+                Object.hasOwn(formData, "new_password") &&
+                Object.hasOwn(formData, "confirm_new_password")
+              ) {
+                const errors = await updatePassword(baseUrl, user_id, formData);
+                if (errors) {
+                  return { errors };
+                }
+
+                return { passwordUpdated: true };
+              }
+
+              return null;
             },
           },
         ],
